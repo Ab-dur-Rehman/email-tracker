bcc28df85f8b96b0523cfba86803b1c056b1dceb
Ab-dur-Rehman <arehman.devs@gmail.com>
2025-04-04 14:56:33 +0500

    Vercel Updations

--- extension/background/service-worker.js
+++ extension/background/service-worker.js
@@ -13,7 +13,7 @@ let activeTrackingSessions = {};
 
 // Configuration for the tracking server
 const CONFIG = {
-  API_ENDPOINT: 'https://api.emailtracker.com',
+  API_ENDPOINT: 'https://email-tracker-virid.vercel.app/',
   TRACKING_PIXEL_PATH: '/pixel',
   LINK_TRACKING_PATH: '/link',
   SYNC_INTERVAL: 5 * 60 * 1000, // 5 minutes
--- extension/content/gmail-integration.js
+++ extension/content/gmail-integration.js
@@ -246,7 +246,7 @@ function extractEmailDetails(composeElement) {
  */
 function insertTrackingPixel(composeElement, trackingId) {
   // Create the tracking pixel HTML
-  const pixelUrl = `https://api.emailtracker.com/pixel/${trackingId}`;
+  const pixelUrl = `https://email-tracker-virid.vercel.app/pixel/${trackingId}`;
   const pixelHtml = `<img src="${pixelUrl}" width="1" height="1" alt="" style="display:none;">`;
   
   // Append the pixel to the email content
@@ -269,7 +269,7 @@ function processLinksForTracking(composeElement, trackingId) {
     
     // Create a tracked URL
     const linkId = `link_${index}`;
-    const trackedUrl = `https://api.emailtracker.com/link/${trackingId}/${linkId}?url=${encodeURIComponent(originalUrl)}`;
+    const trackedUrl = `https://email-tracker-virid.vercel.app/link/${trackingId}/${linkId}?url=${encodeURIComponent(originalUrl)}`;
     
     // Update the link
     link.href = trackedUrl;
